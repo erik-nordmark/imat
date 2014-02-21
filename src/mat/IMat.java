779ab4b9394f6f9c038ff1929ef4691c3e1ee00e
jcberntsson <jcberntsson@gmail.com>
2014-02-21 11:41:19 +0100

    FFAN

---
+++ src/mat/IMat.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mat;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ *
+ * @author Joakim
+ */
+public class IMat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            // Set System L&F
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } 
+        catch (UnsupportedLookAndFeelException e) {
+           // handle exception
+        }
+        catch (ClassNotFoundException e) {
+           // handle exception
+        }
+        catch (InstantiationException e) {
+           // handle exception
+        }
+        catch (IllegalAccessException e) {
+           // handle exception
+        }
+        //IMatModel model = new IMatModel();
+        IMatView view = new IMatView();
+        IMatController controller = new IMatController(view);
+        view.setVisible(true);
+    }
+    
+}
