9979ddeab5be22bfcec35671fbe76b27edc16a4d
hxmn <e.n.nordmark@gmail.com>
2014-02-25 16:17:05 +0100

    cartItem

--- src/mat/CartItem.java
+++ src/mat/CartItem.java
@@ -6,6 +6,8 @@
 
 package mat;
 
+import java.awt.Paint;
+
 /**
  *
  * @author Joakim
@@ -28,22 +30,56 @@ public class CartItem extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        itemLabel = new javax.swing.JLabel();
+        deleteButton = new javax.swing.JToggleButton();
+        itemSpinner = new javax.swing.JSpinner();
+
         setBackground(new java.awt.Color(255, 255, 255));
+        setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, new java.awt.Color(0, 0, 0)));
         setOpaque(false);
+
+        itemLabel.setFont(new java.awt.Font("Avenir Next", 0, 15)); // NOI18N
+        itemLabel.setText("Vara");
+
+        deleteButton.setFont(new java.awt.Font("FontAwesome", 0, 15)); // NOI18N
+        deleteButton.setForeground(new java.awt.Color(255, 0, 0));
+        deleteButton.setText("");
+        deleteButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                deleteButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 242, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(itemLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(itemSpinner, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(2, 2, 2)
+                .addComponent(deleteButton, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 54, Short.MAX_VALUE)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addComponent(itemLabel)
+                .addComponent(deleteButton)
+                .addComponent(itemSpinner, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void deleteButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_deleteButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_deleteButtonActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JToggleButton deleteButton;
+    private javax.swing.JLabel itemLabel;
+    private javax.swing.JSpinner itemSpinner;
     // End of variables declaration//GEN-END:variables
 }
